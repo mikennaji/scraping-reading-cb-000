18a8857836951cd2eee40ff23f0a6c1e470d26e6
Michael Nnaji <mnnaji3@uwo.ca>
2020-07-23 15:11:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,5 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
-flatironschool = doc.css("inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
+flatironschool = doc.css("inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 puts flatironschool
